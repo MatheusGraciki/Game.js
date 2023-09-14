92324d56d0dc448ab3295bccb522ef3d3b89827b
Matheus Graciki <you@example.com>
2023-09-14 00:00:08 -0300

    feature/enhanced-collision-detection

--- js/script.js
+++ js/script.js
@@ -1,70 +1,38 @@
-const mario = document.querySelector('.mario');
-const mysteryBlocks = {
-    myDegrees: document.querySelector('#mystery-block-myDegrees'),
-    aboutMe: document.querySelector('#mystery-block-aboutMe'),
-    myHobbies: document.querySelector('#mystery-block-myHobbies')
-};
+// Select the Player in the game
+const playerMario = document.querySelector('.mario');
 
-const fieldVisibility = {
-    myDegrees: false,
-    aboutMe: false,
-    myHobbies: false
-};
-
-function toggleField(field) {
-    const fieldElement = document.querySelector(`#${field}-field`);
-    fieldElement.style.display = fieldVisibility[field] ? 'none' : 'block';
-    fieldVisibility[field] = !fieldVisibility[field];
-}
-
+// Control variable for Mario's jump
 let isJumping = false;
 
+// Variable to store Mario's horizontal position in percentage
+let marioPositionXPercent = 0;
+
+// Function to execute Mario's jump
 const jump = () => {
     if (!isJumping) {
         isJumping = true;
-        mario.classList.add('jump');
+        playerMario.classList.add('jump');
+        detectCollision();
 
         setTimeout(() => {
-            mario.classList.remove('jump');
-            // Verificar colisão após o pulo
+            playerMario.classList.remove('jump');
             isJumping = false;
+            detectCollision();
         }, 450);
-        
     }
-    detectCollision();
 };
 
-let positionXPercent = 0;
-
-const moveForward = () => {
-    mario.classList.remove('flip');
-    positionXPercent += 1;
-    mario.style.left = positionXPercent + '%';
+// Function to move Mario forward or backward
+const moveMario = (direction) => {
+    if (direction === 'backward') {
+        playerMario.classList.add('flip');
+        marioPositionXPercent -= 1;
+    } else {
+        playerMario.classList.remove('flip');
+        marioPositionXPercent += 1;
+    }
+    playerMario.style.left = marioPositionXPercent + '%';
 };
-
-const moveBackward = () => {
-    mario.classList.add('flip');
-    positionXPercent -= 1;
-    mario.style.left = positionXPercent + '%';
-};
-
-// Função para detectar colisão vertical
-function detectCollision() {
-    const marioRect = mario.getBoundingClientRect();
-
-    Object.keys(mysteryBlocks).forEach((block) => {
-        const mysteryBlockRect = mysteryBlocks[block].getBoundingClientRect();
-
-        if (
-            marioRect.top >= mysteryBlockRect.top &&
-            marioRect.left <= mysteryBlockRect.right &&
-            marioRect.right >= mysteryBlockRect.left
-        ) {
-            console.log(`Colisão detectada no bloco ${block}!`);
-            toggleField(block);
-        }
-    });
-}
 
 document.addEventListener('keydown', (event) => {
     switch (event.key) {
@@ -75,12 +43,12 @@ document.addEventListener('keydown', (event) => {
 
         case 'd':
         case 'ArrowRight':
-            moveForward();
+            moveMario('forward');
             break;
 
         case 'a':
         case 'ArrowLeft':
-            moveBackward();
+            moveMario('backward');
             break;
 
         default:
